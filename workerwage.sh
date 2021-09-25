0dd473e28658f0b405facfdd6ef74a816237b3e3
Shivammishra789 <shivammishra7789@gmail.com>
2021-09-25 22:56:33 +0530

    changed present to 1

--- workerwage.sh
+++ workerwage.sh
@@ -1,8 +1,9 @@
 echo "Welcome to Employee Wage Computation Program"
 random=$((RANDOM%2))
 
-if [ $random-eq 0 ]
-	then echo "Employee is absent"
-elif [ $random -eq 1 ]
-	then echo  "Employee is present"
+if [ $random-eq 1 ]
+	then echo "Employee is present"
+elif [ $random -eq 0 ]
+	then echo  "Employee is abesent"
 fi
+
