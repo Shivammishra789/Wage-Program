fbd22a85c3195a1710db7be5bf3ee618149bb26a
Shivammishra789 <90849543+Shivammishra789@users.noreply.github.com>
2021-09-25 23:00:23 +0530

    if present display one day salary

--- workerwage.sh
+++ workerwage.sh
@@ -1,3 +1,5 @@
+wagePerHr=20
+fullDay=8
 echo "Welcome to Employee Wage Computation Program"
 random=$((RANDOM%2))
 
@@ -5,4 +7,6 @@ if [ $random-eq 0 ]
 	then echo "Employee is absent"
 elif [ $random -eq 1 ]
 	then echo  "Employee is present"
+	oneDaySalary=$(( $wagePerHr*$fullDay ))
+	echo "$oneDaySalary"
 fi
