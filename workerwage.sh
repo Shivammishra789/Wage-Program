45c15f6df980fa750899b06cc7c9ba1f1e1e9441
Shivammishra789 <shivammishra7789@gmail.com>
2021-09-25 23:52:28 +0530

    solved using switch case statement

--- workerwage.sh
+++ workerwage.sh
@@ -2,27 +2,30 @@ wagePerHr=20
 fullDay=8
 partTime=4
 
-absent=0
+
 fullTime=1
 partTime=2
 
 
 echo "Welcome to Employee Wage Computation Program"
 random=$(( RANDOM%3 ))
-echo $random
-if [ $random -eq 1 ]
-	then 
-	     echo "Employee is present"
 
-	    oneDaySalary=$(( $wagePerHr*$fullDay )) 
-elif [ $random -eq 2 ]
-	then 
+case $random in
+	1)
+	     echo "Employee is present"
+	  oneDaySalary=$(( $wagePerHr*$fullDay ))
+	;;
+
+	2)
 	     echo "Employee is part time present"
 	     oneDaySalary=$(( $wagePerHr*$partTime ))
-elif [ $random -eq 0 ]
-	then echo  "Employee is abesent"
-	     oneDaySalary=$(( $wagePerHr*0 ))
-fi
+	;;
+	
+	*)
+		echo  "Employee is abesent"
+		oneDaySalary=$(( $wagePerHr*0 ))
+	;;
+esac
 
 
 echo "Employee salary is $oneDaySalary"
