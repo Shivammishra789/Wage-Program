32e6aedacab992b0ac749452c3d8677d1f79cc9d
Shivammishra789 <shivammishra7789@gmail.com>
2021-09-25 23:43:00 +0530

    add part time employee and wage

--- workerwage.sh
+++ workerwage.sh
@@ -1,16 +1,28 @@
 wagePerHr=20
 fullDay=8
-echo "Welcome to Employee Wage Computation Program"
-random=$(( RANDOM%2 ))
+partTime=4
 
+absent=0
+fullTime=1
+partTime=2
+
+
+echo "Welcome to Employee Wage Computation Program"
+random=$(( RANDOM%3 ))
+echo $random
 if [ $random -eq 1 ]
 	then 
 	     echo "Employee is present"
-	     oneDaySalary=$(( $wagePerHr*$fullDay ))
-	     echo "$oneDaySalary"
 
+	    oneDaySalary=$(( $wagePerHr*$fullDay )) 
+elif [ $random -eq 2 ]
+	then 
+	     echo "Employee is part time present"
+	     oneDaySalary=$(( $wagePerHr*$partTime ))
 elif [ $random -eq 0 ]
 	then echo  "Employee is abesent"
-
+	     oneDaySalary=$(( $wagePerHr*0 ))
 fi
 
+
+echo "Employee salary is $oneDaySalary"
